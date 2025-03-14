b26d1e0f232f7e3b2fcb2aa27df303557c7a0f75
ThomasAlexanderMann <thomasalexander.mann@dedalus.com>
2025-03-14 17:49:46 +0000

    feat: Telescope - added horizontal layout and new key mappings

--- init.lua
+++ init.lua
@@ -423,12 +423,19 @@ require('lazy').setup({
               ['<C-Up>'] = require('telescope.actions').cycle_history_prev,
             },
           },
-          ['layout_strategy'] = 'vertical',
+          ['layout_strategy'] = 'horizontal',
           ['sorting_strategy'] = 'ascending',
           ['layout_config'] = {
             vertical = {
               height = 0.9,
               preview_cutoff = 10,
+              prompt_position = 'bottom',
+              width = 0.9,
+            },
+            horizontal = {
+              height = 0.9,
+              preview_cutoff = 10,
+              preview_width = 0.5,
               prompt_position = 'bottom',
               width = 0.9,
             },
@@ -450,15 +457,17 @@ require('lazy').setup({
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sgb', builtin.git_branches, { desc = '[S]earch [G]it [B]ranches' })
+      vim.keymap.set('n', '<leader>sgc', builtin.git_commits, { desc = '[S]earch [G]it [C]ommits' })
+      vim.keymap.set('n', '<leader>sgh', builtin.git_bcommits, { desc = '[S]earch [G]it current buffer [H]istory' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader><leader>', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
