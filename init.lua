955575ad491fb1db675e8b772190dcc01c8922f5
ThomasAlexanderMann <thomasalexander.mann@dedalus.com>
2025-04-05 18:18:38 +0100

    refactor: moved JS console log utils into /lua and imported

--- init.lua
+++ init.lua
@@ -1035,32 +1035,7 @@ require('lazy').setup({
   },
 })
 
-function JS_console_log_from_yank_register()
-  local yankRegister = vim.fn.getreg '0'
-  local yankRegisterNoNewLines = string.gsub(yankRegister, '\n', '')
-  local newline = 'console.log("' .. yankRegisterNoNewLines .. '", ' .. yankRegisterNoNewLines .. ');'
-  local currentLineNumber = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.api.nvim_buf_set_lines(0, currentLineNumber, currentLineNumber, true, { newline })
-end
-vim.api.nvim_set_keymap('n', ',l', '<cmd>lua JS_console_log_from_yank_register()<cr>', {})
+require 'custom/utils/js_console_logs'
 
-function JS_console_log_JSON_from_yank_register()
-  local yankRegister = vim.fn.getreg '0'
-  local yankRegisterNoNewLines = string.gsub(yankRegister, '\n', '')
-  local newline = 'console.log("' .. yankRegisterNoNewLines .. '", JSON.stringify(' .. yankRegisterNoNewLines .. '));'
-  local currentLineNumber = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.api.nvim_buf_set_lines(0, currentLineNumber, currentLineNumber, true, { newline })
-end
-vim.api.nvim_set_keymap('n', ',j', '<cmd>lua JS_console_log_JSON_from_yank_register()<cr>', {})
-
-function JS_console_log_type_from_yank_register()
-  local yankRegister = vim.fn.getreg '0'
-  local yankRegisterNoNewLines = string.gsub(yankRegister, '\n', '')
-  local newline = 'console.log("' .. yankRegisterNoNewLines .. '", "**", typeof ' .. yankRegisterNoNewLines .. ', "**" ,' .. yankRegisterNoNewLines .. ');'
-  local currentLineNumber = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.api.nvim_buf_set_lines(0, currentLineNumber, currentLineNumber, true, { newline })
-end
-vim.api.nvim_set_keymap('n', ',t', '<cmd>lua JS_console_log_type_from_yank_register()<cr>', {})
-
--- The line beneath this is called `modeline`. See `:help modeline`
+-- function JS_console_log_from_yank_register(-- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
