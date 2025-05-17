b19e0f24fe404366c04e99b73a254eafa36c387f
ThomasAlexanderMann <thomasalexander.mann@dedalus.com>
2025-05-17 23:21:31 +0100

    feat: use lualine instead of mini.statusline

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,69 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          always_show_tabline = true,
+          globalstatus = true,
+          refresh = {
+            statusline = 100,
+            tabline = 100,
+            winbar = 100,
+          },
+        },
+        sections = {
+          lualine_a = {
+            {
+              'mode',
+              fmt = function(str)
+                return str:sub(1, 1)
+              end,
+            },
+          },
+          lualine_b = {
+            {
+              'branch',
+              fmt = function(branch)
+                if branch:len() < 12 then
+                  return branch
+                end
+                return branch:sub(0, 12) .. '...'
+              end,
+            },
+            'diff',
+            'diagnostics',
+          },
+          lualine_c = { { 'filename', path = 1 } },
+          lualine_x = { 'filename' },
+          lualine_y = { 'filetype' },
+
+          lualine_z = {},
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
+      }
+    end,
+  },
+}
