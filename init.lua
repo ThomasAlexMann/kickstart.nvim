e345792ce42b4fe6dd4c7d79f5086353bcc4d4f3
ThomasAlexanderMann <thomasalexander.mann@dedalus.com>
2025-05-18 21:35:22 +0100

    feat: use prettier as formatter for jsonc files

--- init.lua
+++ init.lua
@@ -808,6 +808,7 @@ require('lazy').setup({
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
         typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         json = { 'prettierd', 'prettier', stop_after_first = true },
+        jsonc = { 'prettierd', 'prettier', stop_after_first = true },
         html = { 'prettierd', 'prettier', stop_after_first = true },
         css = { 'prettierd', 'prettier', stop_after_first = true },
       },
