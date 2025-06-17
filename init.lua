ca08435f37bc688bc919be5f5eef2000aace9ac4
ThomasAlexanderMann <thomasalexander.mann@dedalus.com>
2025-06-17 21:39:51 +0100

    feat: added key binding for showing full error in floating window.

--- init.lua
+++ init.lua
@@ -627,6 +627,7 @@ require('lazy').setup({
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('E', vim.diagnostic.open_float, 'Show [E]rror in floating window')
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
