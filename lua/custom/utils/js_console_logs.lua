4ac6982b7e0b18f2516da238a3153f3eb6ed6b3d
thomasmann <thomasalexander.mann@dedalus.eu>
2026-04-20 09:22:35 +0100

    feat: swap to using alt key for console log helpers

--- lua/custom/utils/js_console_logs.lua
+++ lua/custom/utils/js_console_logs.lua
@@ -3,7 +3,7 @@ function JS_console_log_blank()
   local currentLineNumber = unpack(vim.api.nvim_win_get_cursor(0))
   vim.api.nvim_buf_set_lines(0, currentLineNumber, currentLineNumber, true, { newline })
 end
-vim.api.nvim_set_keymap('n', ',b', '<cmd>lua JS_console_log_blank()<cr>', {})
+vim.api.nvim_set_keymap('n', '<M-b>', '<cmd>lua JS_console_log_blank()<cr>', {})
 -- TODO - place the cursor at the "" in the console.log
 
 function JS_console_log_from_yank_register()
@@ -13,7 +13,7 @@ function JS_console_log_from_yank_register()
   local currentLineNumber = unpack(vim.api.nvim_win_get_cursor(0))
   vim.api.nvim_buf_set_lines(0, currentLineNumber, currentLineNumber, true, { newline })
 end
-vim.api.nvim_set_keymap('n', ',l', '<cmd>lua JS_console_log_from_yank_register()<cr>', {})
+vim.api.nvim_set_keymap('n', '<M-l>', '<cmd>lua JS_console_log_from_yank_register()<cr>', {})
 
 function JS_console_log_JSON_from_yank_register()
   local yankRegister = vim.fn.getreg '0'
@@ -22,7 +22,7 @@ function JS_console_log_JSON_from_yank_register()
   local currentLineNumber = unpack(vim.api.nvim_win_get_cursor(0))
   vim.api.nvim_buf_set_lines(0, currentLineNumber, currentLineNumber, true, { newline })
 end
-vim.api.nvim_set_keymap('n', ',j', '<cmd>lua JS_console_log_JSON_from_yank_register()<cr>', {})
+vim.api.nvim_set_keymap('n', '<M-j>', '<cmd>lua JS_console_log_JSON_from_yank_register()<cr>', {})
 
 function JS_console_log_type_from_yank_register()
   local yankRegister = vim.fn.getreg '0'
@@ -31,4 +31,4 @@ function JS_console_log_type_from_yank_register()
   local currentLineNumber = unpack(vim.api.nvim_win_get_cursor(0))
   vim.api.nvim_buf_set_lines(0, currentLineNumber, currentLineNumber, true, { newline })
 end
-vim.api.nvim_set_keymap('n', ',t', '<cmd>lua JS_console_log_type_from_yank_register()<cr>', {})
+vim.api.nvim_set_keymap('n', '<M-t>', '<cmd>lua JS_console_log_type_from_yank_register()<cr>', {})
