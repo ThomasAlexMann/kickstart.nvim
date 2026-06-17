4176f7a5a796194ca3d6285c0f2a00d77d6e41d1
thomasmann <thomasalexander.mann@dedalus.eu>
2026-06-17 14:53:55 +0100

    feat: added jsx comments - not working for me but is for Bruno

--- lua/custom/plugins/comment.lua
+++ lua/custom/plugins/comment.lua
@@ -1,6 +1,7 @@
 return {
   'numToStr/Comment.nvim',
   opts = {
-    -- add any options here
+    enable_autocmd = false,
+    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
   },
 }
