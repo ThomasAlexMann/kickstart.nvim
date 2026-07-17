ffb91f56935df1dc6329c2551dff9b2c7a2f9f9f
thomasmann <thomasalexander.mann@dedalus.eu>
2026-07-17 16:43:45 +0100

    build: update lazy packages

--- lua/custom/plugins/comment.lua
+++ lua/custom/plugins/comment.lua
@@ -1,7 +1,7 @@
 return {
   'numToStr/Comment.nvim',
-  opts = {
-    enable_autocmd = false,
-    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-  },
+  -- opts = {
+  --   enable_autocmd = false,
+  --   pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  -- },
 }
